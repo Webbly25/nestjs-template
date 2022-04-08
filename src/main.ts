da6c5fd1f3778e6b18ac6387778a9f276388a926
Ryan W <rywebb1998@gmail.com>
2022-04-08 15:46:51 +0100

    Print url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	const port = 3000;
+	await app.listen(port);
+
+	const url = await app.getUrl();
+	console.log(`Running at ${url}`);
 }
 bootstrap();
