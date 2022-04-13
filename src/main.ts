06c0cec59a67a0cba0091eb80dce5d91679e0c84
Ryan W <79157185+Webbly25@users.noreply.github.com>
2022-04-13 14:36:04 +0100

    Specify type of nest application

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
 	const port = 3000;
 	await app.listen(port);
